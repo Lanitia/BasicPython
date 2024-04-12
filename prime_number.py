5424ec687f514c393dd5efad925d473dbc0c4a30
Lintia <s.kakinuma.24.4.1326@4nonome.com>
2024-04-12 20:54:49 +0900

    制御構文の答え

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,16 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+
+test = int(b) #この値を変更してください
+
+
+for i in range(2, int(test ** 0.5)+1) :
+    if i == int(test ** 0.5) :
+        print(f"{test}は素数")
+        break
+    elif test // i * i == test :
+        print(f"{test}は素数でない")
+        print(f"{i}で割り切れる")
+        break
+
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,13 @@
 from math import sin
+from math import pi
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+a = 0
+b = 0.5 * pi
+N = 100
+h = (b - a) / N
+s = sum([sin(a + (i - 1) * h) + sin(a + i * h) for i in range(1, N+1)])
+
+print(h / 2 * s)
