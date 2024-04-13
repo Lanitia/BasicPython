2396f2a8337c5293e24525a280bc8fbc20d4845a
Lintia <s.kakinuma.24.4.1326@4nonome.com>
2024-04-13 11:48:20 +0900

    prime_numberの修正

--- prime_number.py
+++ prime_number.py
@@ -3,21 +3,23 @@ b = input("bの値を入力: ")
 
 # TODO
 
-test = int(b) #この値を変更してください
-
 def isPrime(n) :
-    if str(int(n)) != n or abs(int(n)) != int(n) :
+    if str(int(n)) != n or abs(int(n)) != int(n) or int(n) == 1:
         print("type error : unexpected type")
-        print("正の整数を入力してください")
+        print("2以上の正の整数を入力してください")
     else :
         n = int(n)
-        for i in range(2, int(n ** 0.5)+1) :
-            if i == int(n ** 0.5) :
-                return True
-                break
-            elif n % i == 0 :
-                return False
-                break
+        jud = None
+        if n == 2 :
+            return True
+        else :
+            for i in range(2, int(n ** 0.5)+1) :
+                if n % i == 0 :
+                    jud = True
+                    break
+            else :
+                jud = False
+            return jud
 
 print(isPrime(a))
 print(isPrime(b))
