0c79812f824e4a98eb3c2110d72293b05eb1b211
Lintia <s.kakinuma.24.4.1326@4nonome.com>
2024-04-13 10:42:51 +0900

    制御構文（修正1）

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,5 +1,6 @@
 # TODO
+
 e = 1
 while (1 + e) > 1 :
     e /= 2
-print(e)
+print(e*2)
--- prime_number.py
+++ prime_number.py
@@ -10,8 +10,7 @@ for i in range(2, int(test ** 0.5)+1) :
     if i == int(test ** 0.5) :
         print(f"{test}は素数")
         break
-    elif test // i * i == test :
+    elif test % i == 0 :
         print(f"{test}は素数でない")
         print(f"{i}で割り切れる")
         break
-
